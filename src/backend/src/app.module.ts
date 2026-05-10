09c02d58eb9e30b1caa8ddaf1e73f5042cf0fd5a
Leano Baba <leano.baba@ufam.edu.br>
2026-05-10 16:58:06 -0400

    SCRUM-29 add modules for authentication, user management, academic data, and reporting

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { PerfisModule } from './modules/perfis/perfis.module';
+import { DadosAcademicosModule } from './modules/dados-academicos/dados-academicos.module';
+import { CurriculosModule } from './modules/curriculos/curriculos.module';
+import { HistoricoModule } from './modules/historico/historico.module';
+import { ProjetosModule } from './modules/projetos/projetos.module';
+import { DispositivosModule } from './modules/dispositivos/dispositivos.module';
+import { AcessoLaboratorioModule } from './modules/acesso-laboratorio/acesso-laboratorio.module';
+import { StatusAcademicoModule } from './modules/status-academico/status-academico.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { RelatoriosModule } from './modules/relatorios/relatorios.module';
+import { NotificacoesModule } from './modules/notificacoes/notificacoes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsuariosModule,
+    PerfisModule,
+    DadosAcademicosModule,
+    CurriculosModule,
+    HistoricoModule,
+    ProjetosModule,
+    DispositivosModule,
+    AcessoLaboratorioModule,
+    StatusAcademicoModule,
+    DashboardModule,
+    RelatoriosModule,
+    NotificacoesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
